8076a28d714a3fc9f2eb37de0ffcb97713aa3f2a
Quid-solo <eedo542363@outlook.com>
2025-04-12 00:03:49 +0530

    Add basic calculation capability

---
+++ Frontend/script.js
@@ -0,0 +1,41 @@
+
+// Wait until the DOM is ready
+document.addEventListener("DOMContentLoaded", () => {
+    const inputOutputField = document.querySelector("#inputOutputfield"); // The textarea
+    const buttons = document.querySelectorAll(".inputbutton"); // All input buttons
+    // const clearButton = document.getElementById("clearButton"); // Clear button
+    
+
+    inputOutputField.addEventListener("keydown", (event) => {
+        event.preventDefault(); // Stops keyboard input
+    });
+
+    const complex = false;
+    // Add event listeners to each input button
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const value = button.dataset.value; // Get the button's value
+            if(button.className === "func") complex = true;
+            if(value === "backspace") inputOutputField.value = inputOutputField.value.slice(0,-1);
+            else if(value === "delete") inputOutputField.value = "";
+            else if(value === "solve"){
+                if(complex === false){
+                    const result = eval(inputOutputField.value);
+                    inputOutputField.value = result; 
+                }
+            }
+
+            else inputOutputField.value += value; // Append the value to the textarea
+
+            
+            });
+    });
+
+                // Clear the input field
+                // clearButton.addEventListener("click", () => {
+                //     inputField.value = ""; // Clear the textarea
+                // });
+});
+
+
+
